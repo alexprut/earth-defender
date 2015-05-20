758c985bf74419dcf004b5d64a06bc5363298984
Alex Prut <1648497+alexprut@users.noreply.github.com>
2015-05-20 19:08:45 +0200

    Refactoring in OOP

--- js/main.js
+++ js/main.js
@@ -1,71 +1,121 @@
-var scene,
-    camera,
-    renderer,
-    earth,
-    moon,
-    spaceShip,
-    controls,
-    guiParams,
-    stats,
-    gui;
-
-var createEarth = function () {
+var Earth = function () {
+};
+Earth.prototype.constructor = Earth;
+Earth.prototype.create = function () {
     var geometry = new THREE.SphereGeometry(40, 20, 20);
     var material = new THREE.MeshBasicMaterial({color: 0x0000ff});
 
     return new THREE.Mesh(geometry, material);
 };
 
-var createMoon = function () {
+
+var Moon = function () {
+};
+Moon.prototype.constructor = Moon;
+Moon.prototype.create = function () {
     var geometry = new THREE.SphereGeometry(20, 15, 15);
     var material = new THREE.MeshBasicMaterial({color: 0x0000ff});
 
     return new THREE.Mesh(geometry, material);
 };
 
-var createSpaceShip = function () {
+
+var SpaceShip = function () {
+};
+SpaceShip.prototype.constructor = SpaceShip;
+SpaceShip.prototype.create = function () {
     var material = new THREE.MeshBasicMaterial({color: 0x0000ff});
     var geometry = new THREE.CylinderGeometry(1, 5, 20, 4);
 
     return new THREE.Mesh(geometry, material);
 };
 
-var init = function () {
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-    renderer = new THREE.WebGLRenderer({antialias: true});
+var Meteorite = function () {
+};
+Meteorite.prototype.constructor = Meteorite;
+Meteorite.prototype.create = function () {
+    var geometry = new THREE.SphereGeometry(5, 10, 10);
+    var material = new THREE.MeshBasicMaterial({color: 0x0000ff});
+
+    return new THREE.Mesh(geometry, material);
+};
+
+
+var Game = function () {
+    this.earth = null;
+    this.moon = null;
+    this.scene = null;
+    this.camera = null;
+    this.renderer = null;
+    this.spaceShip = null;
+    this.meteorites = null;
+    this.controls = null;
+    this.gui = null;
+    this.stats = null;
+};
+Game.prototype.constructor = Game;
+Game.prototype.initCamera = function () {
+    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 500;
+
+    return camera;
+};
+Game.prototype.initEarth = function () {
+    var earth = new Earth();
+    earth = earth.create();
+    earth.rotation.z = 10 * Math.PI / 180;
+
+    return earth;
+};
+Game.prototype.initRender = function () {
+    var renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
 
-    earth = createEarth();
-    spaceShip = createSpaceShip();
-    var tmpMoon = createMoon();
-
-    tmpMoon.position.x = 200;
-    moon = new THREE.Object3D();
-
-    moon.add(tmpMoon);
-
-    scene.add(moon);
-    scene.add(earth);
-    scene.add(spaceShip);
-
-    controls = new THREE.OrbitControls(camera);
-
+    return renderer;
+};
+Game.prototype.initSpaceShip = function () {
+    var spaceShip = new SpaceShip();
+    spaceShip = spaceShip.create();
     spaceShip.position.x = 100;
     spaceShip.rotation.z = 90 * Math.PI / 180;
 
-    camera.position.z = 500;
-
-    createGui();
-    initStats();
-
-    render();
+    return spaceShip;
 };
+Game.prototype.initMoon = function () {
+    var tmpMoon = new Moon();
+    tmpMoon = tmpMoon.create();
+    tmpMoon.position.x = 200;
 
-var createGui = function () {
-    gui = new dat.GUI();
+    var moon = new THREE.Object3D();
+    moon.add(tmpMoon);
+
+    return moon;
+};
+Game.prototype.initMeteorites = function (numMeteorites) {
+    var meteorites = new THREE.Object3D();
+    for (var i = 0; i < numMeteorites; i++) {
+        var meteorite = new Meteorite().create();
+        meteorite.position.x = Math.random() * 1000 - Math.random() * 1000;
+        meteorite.position.y = Math.random() * 1000 - Math.random() * 1000;
+        meteorite.position.z = Math.random() * 1000 - Math.random() * 1000;
+
+        meteorites.add(meteorite);
+
+    }
+
+    return meteorites;
+};
+Game.prototype.initStats = function () {
+    var stats = new Stats();
+    stats.setMode(0);
+
+    document.body.appendChild(stats.domElement);
+
+    return stats;
+};
+Game.prototype.initGui = function () {
+    var gui = new dat.GUI();
 
     guiParams = {
         wireframe: false
@@ -73,33 +123,72 @@ var createGui = function () {
 
     var debugFolder = gui.addFolder('Debug');
 
-    debugFolder.add(guiParams, 'wireframe').listen().onFinishChange(function () {
-        earth.material.wireframe = guiParams.wireframe;
-        spaceShip.material.wireframe = guiParams.wireframe;
-        moon.children[0].material.wireframe = guiParams.wireframe;
-    });
+    debugFolder.add(guiParams, 'wireframe').listen().onFinishChange((function () {
+        this.earth.material.wireframe = guiParams.wireframe;
+        this.spaceShip.material.wireframe = guiParams.wireframe;
+        this.moon.children[0].material.wireframe = guiParams.wireframe;
+    }).bind(this));
+
+    return gui;
 };
+Game.prototype.init = function () {
+    this.renderer = this.initRender();
+    this.scene = new THREE.Scene();
+    this.camera = this.initCamera();
+    this.stats = this.initStats();
+    this.controls = new THREE.OrbitControls(this.camera);
+    this.earth = this.initEarth();
+    this.moon = this.initMoon();
+    this.spaceShip = this.initSpaceShip();
+    this.meteorites = this.initMeteorites(5);
 
-var initStats = function () {
-    stats = new Stats();
-    stats.setMode(0);
+    this.scene.add(this.moon);
+    this.scene.add(this.earth);
+    this.scene.add(this.spaceShip);
+    this.scene.add(this.meteorites);
 
-    stats.domElement.style.position = 'absolute';
-    stats.domElement.style.left = '0px';
-    stats.domElement.style.top = '0px';
+    this.initGui();
 
-    document.body.appendChild(stats.domElement);
+    document.body.appendChild(this.renderer.domElement);
+    this.render();
 };
+Game.prototype.render = function () {
+    requestAnimationFrame(this.render.bind(this));
 
-var render = function () {
-    requestAnimationFrame(render);
+    this.stats.begin();
+    this.renderer.render(this.scene, this.camera);
+    this.stats.end();
 
-    stats.begin();
-    renderer.render(scene, camera);
-    stats.end();
     // Rotate the sphere
-    earth.rotation.y += 0.001;
-    moon.rotation.y += 0.0005;
+    this.earth.rotation.y += 0.001;
+    this.moon.rotation.y += 0.0005;
+
+    this.meteorites.children.forEach((function (currentValue, index) {
+        var moveFactor = 1;
+        var meteorite = currentValue;
+        var x = meteorite.position.x;
+        var y = meteorite.position.y;
+        var z = meteorite.position.z;
+
+        if (x < 0) {
+            meteorite.position.x += moveFactor;
+        } else {
+            meteorite.position.x -= moveFactor;
+        }
+
+        if (y < 0) {
+            meteorite.position.y += moveFactor;
+        } else {
+            meteorite.position.y -= moveFactor;
+        }
+
+        if (z < 0) {
+            meteorite.position.z += moveFactor;
+        } else {
+            meteorite.position.z -= moveFactor;
+        }
+    }).bind(this));
 };
 
-init();
+var game = new Game();
+game.init();
