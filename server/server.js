495b71d57b3a578112825dcf3a355a05b814ac42
P.Alex <web.is.art@p-alex.com>
2016-07-26 21:09:47 +0200

    Server Publish/Subscribe design pattern

--- server/server.js
+++ server/server.js
@@ -1,6 +1,79 @@
+// Library for generating random strings
 var randomString = require("randomstring");
-var clients = [];
-var gameRoom = [];
+
+// Pubblish / Subscribe design pattern
+function PublishSubscribe() {
+    var subscribers = {
+        topics: [],
+        token: -1,
+        add: function (event, func) {
+            if (!this.topics[event]) {
+                this.topics[event] = [];
+            }
+
+            this.topics[event][++this.token] = func;
+
+            return (this.token).toString();
+        },
+        remove: function (token) {
+            for (var event in this.topics) {
+                if (this.topics[event][token]) {
+                    this.topics[event].splice(token, 1);
+
+                    return true;
+                }
+            }
+
+            return false;
+        },
+        broadcast: function (event, args) {
+            for (var subscriber in this.topics[event]) {
+                this.topics[event][subscriber](args);
+            }
+        }
+    };
+
+    return {
+        subscribe: function (event, func) {
+            return subscribers.add(event, func);
+        },
+        unsubscribe: function (token) {
+            return subscribers.remove(token);
+        },
+        publish: function (event, args) {
+            subscribers.broadcast(event, args);
+        }
+    }
+};
+
+function Clients() {
+    var _clients = [];
+
+    function remove(clientId) {
+        var index = _clients.indexOf(clientId);
+        if (index != -1) {
+            _clients.splice(index, 1)
+        }
+    }
+
+    function add(clientId) {
+        _clients.push(clientId);
+    }
+
+    function getAll () {
+        return _clients;
+    }
+
+    return {
+        add: add,
+        remove: remove,
+        getAll: getAll
+    }
+}
+
+
+var clients = new Clients();
+var gameRoom = new PublishSubscribe();
 
 
 /////////////////////////////////////////////////////
@@ -9,30 +82,34 @@ var gameRoom = [];
 var server = require('http').createServer()
     , url = require('url')
     , WebSocketServer = require('ws').Server
-    , wss = new WebSocketServer({ server: server })
+    , wss = new WebSocketServer({server: server})
     , express = require('express')
     , app = express()
     , port = 8888;
 
 app.use(function (req, res) {
-    res.send({ msg: "Earth Defender: Game Server" });
+    res.send({msg: "Earth Defender: Game Server"});
 });
 
 wss.on('connection', function connection(ws) {
-    var clientId = randomString.generate(10);
-    clients.push(clientId);
+    const clientId = randomString.generate(10);
+
+    clients.add(clientId);
     ws.send(JSON.stringify({'clientId': clientId}));
 
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
         console.log(message);
-        console.log(clients);
+        console.log(clients.getAll());
     });
 
     ws.on('close', function (error) {
-       console.log("closed connection client: " + clientId);
+        clients.remove(clientId);
+        console.log("closed connection client: " + clientId);
     });
 });
 
 server.on('request', app);
-server.listen(port, function () { console.log('Listening on ' + server.address().port) });
+server.listen(port, function () {
+    console.log('Listening on ' + server.address().port)
+});
