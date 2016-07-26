5402578984d6b8c1a175792f976f4d104dcf08d0
P.Alex <web.is.art@p-alex.com>
2016-07-26 17:57:13 +0200

    Refactoring

--- server/server.js
+++ server/server.js
@@ -1,3 +1,11 @@
+var randomString = require("randomstring");
+var clients = [];
+var gameRoom = [];
+
+
+/////////////////////////////////////////////////////
+// WEBSOCKET SERVER & HTTP SERVER
+/////////////////////////////////////////////////////
 var server = require('http').createServer()
     , url = require('url')
     , WebSocketServer = require('ws').Server
@@ -11,12 +19,19 @@ app.use(function (req, res) {
 });
 
 wss.on('connection', function connection(ws) {
+    var clientId = randomString.generate(10);
+    clients.push(clientId);
+    ws.send(JSON.stringify({'clientId': clientId}));
 
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
+        console.log(message);
+        console.log(clients);
     });
 
-    ws.send('something');
+    ws.on('close', function (error) {
+       console.log("closed connection client: " + clientId);
+    });
 });
 
 server.on('request', app);
