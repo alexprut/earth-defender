de4674fa0e15e0a1c6acb9c5c4d23b508ab04070
P.Alex <web.is.art@p-alex.com>
2016-08-02 14:53:53 +0200

    DOM handler uncoupled, basic game UI

--- client/js/gameClient.js
+++ client/js/gameClient.js
@@ -1,48 +1,54 @@
 var websocket;
 var player_socket;
 init();
-      function init() {
-              connect();
-      };
 
-      function connect()
-      {
-          websocket = new WebSocket('ws://127.0.0.1:8888/websocket');
-          websocket.onopen = function(evt) { onOpen(evt) }; 
-          websocket.onclose = function(evt) { onClose(evt) }; 
-          websocket.onmessage = function(evt) { onMessage(evt) }; 
-          websocket.onerror = function(evt) { onError(evt) };
-      };
-      
-      
-      
-      function disconnect() {
-          websocket.close();
-      };
+function init() {
+    connect();
+};
 
-      function sendTxt(txt) {
-          if(websocket.readyState == websocket.OPEN){
-              websocket.send(txt);
-              console.log('sending: ' + txt);
-          } else {
-               console.log('websocket is not connected'); 
-          };
-      };
+function connect() {
+    websocket = new WebSocket('ws://127.0.0.1:8888/websocket');
+    websocket.onopen = function (evt) {
+        onOpen(evt)
+    };
+    websocket.onclose = function (evt) {
+        onClose(evt)
+    };
+    websocket.onmessage = function (evt) {
+        onMessage(evt)
+    };
+    websocket.onerror = function (evt) {
+        onError(evt)
+    };
+};
 
-      function onOpen(evt) {
-          console.log('onOpen');
-          game.updateNofPlayers();
-      };  
+function disconnect() {
+    websocket.close();
+};
 
-      function onClose(evt) { 
-          console.log('onClose');
-      };  
+function sendTxt(txt) {
+    if (websocket.readyState == websocket.OPEN) {
+        websocket.send(txt);
+        console.log('sending: ' + txt);
+    } else {
+        console.log('websocket is not connected');
+    }
+};
 
-      function onMessage(evt) { 
-          player_socket = evt.data;
-          game.updateNofPlayers();
-      };  
+function onOpen(evt) {
+    console.log('onOpen');
+    game.updateNofPlayers();
+};
 
-      function onError(evt) {
-          console.log('onError' + evt.data );
-      };
+function onClose(evt) {
+    console.log('onClose');
+};
+
+function onMessage(evt) {
+    player_socket = evt.data;
+    game.updateNofPlayers();
+};
+
+function onError(evt) {
+    console.log('onError' + evt.data);
+};
