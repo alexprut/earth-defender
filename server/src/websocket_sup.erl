8a5a524d0b4cc277ac0703668e97de9d5a10ef68
Alex Prut <mail@alexprut.com>
2016-08-23 23:16:13 +0200

    Add supervisor/gen_serve to global_rooms_state, fix some bugs

--- server/src/websocket_sup.erl
+++ server/src/websocket_sup.erl
@@ -17,5 +17,14 @@ start_link() ->
 %% supervisor.
 
 init([]) ->
-  Procs = [],
+  Procs = [
+    {
+      global_rooms_state,
+      {global_rooms_state, start_link, []},
+      permanent,
+      infinity,
+      worker,
+      dynamic
+    }
+  ],
   {ok, {{one_for_one, 10, 10}, Procs}}.
